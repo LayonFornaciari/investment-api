44d47c753c8e67e43c465108e15e1d893ab95f87
Layon Fornaciari <layon.fornaciari@gmail.com>
2025-12-17 19:39:28 +0000

    Adiciando middleware

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import Portfolio, Asset  # noqa: F401
 from app.routers import portfolios, assets, market
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +13,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Investment Portfolio API",
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(portfolios.router)
